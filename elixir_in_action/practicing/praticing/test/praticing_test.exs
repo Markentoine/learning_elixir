6203130e27ca68fee615969528ec60f05553c8c9
Jerome Toulouse <monsieur.toulouse@gmail.com>
2019-08-06 18:17:13 +0200

    wip.

---
+++ elixir_in_action/practicing/praticing/test/praticing_test.exs
@@ -0,0 +1,8 @@
+defmodule PraticingTest do
+  use ExUnit.Case
+  doctest Praticing
+
+  test "greets the world" do
+    assert Praticing.hello() == :world
+  end
+end
--- elixir_in_action/practicing/read_lines.ex
+++ elixir_in_action/practicing/read_lines.ex
@@ -1,5 +1,10 @@
-def large_lines!(path) do
-  File.stream!(path)
-  |> Stream.map(&String.replace(&1, "\n", ""))
-  |> Enum.filter(&(String.length(&1) > 80))
+defmodule ReadLines do
+    def large_lines!(path) do
+        File.stream!(path)
+        |> Stream.map(&String.replace(&1, "\n", ""))
+        |> Enum.filter(&(String.length(&1) > 80))
+    end
+
+
 end
+
